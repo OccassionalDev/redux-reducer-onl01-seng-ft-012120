f8dff5785d39b435d54ec6ffec5852b6095e600e
Erica Schimmel <ericaschimmel01@gmail.com>
2020-06-09 22:27:42 +0000

    Automatically backed up by Learn

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,12 @@
 export function manageFriends(state, action){
+  
+  switch(action.type) {
+    case "ADD_FRIEND":
+      return { friend: {
+        
+        }
+      }
+      break
+      
+  }
 }
